a77dae56e808db3b0f964ee5d12a26cf8b612602
Chovry <1271665937@qq.com>
2023-01-16 17:12:31 +0800

    Add sub function

--- hello.c
+++ hello.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+int sub(int a, int b)
+{
+	return a - b;
+}
+
 int main()
 {
+	int a = 20;
+	int b = 10;
+
 	printf("Hello, World!\n");
+	printf("%d - %d = %d\n", a, b, sub(a, b));
+
 	return 0;
 }
